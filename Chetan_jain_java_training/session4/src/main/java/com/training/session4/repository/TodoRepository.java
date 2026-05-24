5d8ef463fe8536c6f13d5a8a6f54d8b121114901
mastercj26 <cj450483@gmail.com>
2026-05-25 02:02:06 +0530

    feat: add JpaRepository for Todo entity

---
+++ Chetan_jain_java_training/session4/src/main/java/com/training/session4/repository/TodoRepository.java
@@ -0,0 +1,9 @@
+package com.training.session4.repository;
+
+import com.training.session4.model.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+}
