a01545cce8ba9d563fcaeee28a8fd36ba4e39d39
mastercj26 <cj450483@gmail.com>
2026-05-25 02:01:26 +0530

    feat: add application configuration and main entry point

---
+++ Chetan_jain_java_training/session4/src/main/java/com/training/session4/Session4Application.java
@@ -0,0 +1,12 @@
+package com.training.session4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Session4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Session4Application.class, args);
+    }
+}
