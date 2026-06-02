687815157e2700f3369d7c50d96f55f828862d1f
mastercj26 <cj450483@gmail.com>
2026-06-02 21:13:21 +0530

    feat: add TodoRepository interface

---
+++ Chetan_jain_java_training/session4/src/main/java/com/training/todo/repository/TodoRepository.java
@@ -0,0 +1,9 @@
+package com.training.todo.repository;
+
+import com.training.todo.model.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+}
