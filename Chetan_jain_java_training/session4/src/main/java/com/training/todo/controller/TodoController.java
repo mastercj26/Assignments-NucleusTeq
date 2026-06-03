9688d09158a9b18d2e1d10f18c08a3c0c62a9eac
mastercj26 <cj450483@gmail.com>
2026-06-04 04:27:22 +0530

    feat: add logging to TodoController

--- Chetan_jain_java_training/session4/src/main/java/com/training/todo/controller/TodoController.java
+++ Chetan_jain_java_training/session4/src/main/java/com/training/todo/controller/TodoController.java
@@ -4,6 +4,8 @@ import com.training.todo.dto.TodoDTO;
 import com.training.todo.model.Todo;
 import com.training.todo.service.TodoService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,7 @@ import java.util.List;
 @RequestMapping("/todos")
 public class TodoController {
 
+    private static final Logger logger = LoggerFactory.getLogger(TodoController.class);
     private final TodoService todoService;
 
     public TodoController(TodoService todoService) {
@@ -22,30 +25,35 @@ public class TodoController {
 
     @PostMapping
     public ResponseEntity<Todo> createTodo(@Valid @RequestBody TodoDTO todoDTO) {
+        logger.info("Received request to create todo: " + todoDTO.getTitle());
         Todo createdTodo = todoService.createTodo(todoDTO);
         return new ResponseEntity<>(createdTodo, HttpStatus.CREATED);
     }
 
     @GetMapping
     public ResponseEntity<List<Todo>> getAllTodos() {
+        logger.info("Received request to fetch all todos");
         List<Todo> todos = todoService.getAllTodos();
         return new ResponseEntity<>(todos, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Todo> getTodoById(@PathVariable Long id) {
+        logger.info("Received request to fetch todo with id: " + id);
         Todo todo = todoService.getTodoById(id);
         return new ResponseEntity<>(todo, HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Todo> updateTodo(@PathVariable Long id, @Valid @RequestBody TodoDTO todoDTO) {
+        logger.info("Received request to update todo with id: " + id);
         Todo updatedTodo = todoService.updateTodo(id, todoDTO);
         return new ResponseEntity<>(updatedTodo, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteTodo(@PathVariable Long id) {
+        logger.info("Received request to delete todo with id: " + id);
         todoService.deleteTodo(id);
         return new ResponseEntity<>("Todo deleted successfully", HttpStatus.OK);
     }
