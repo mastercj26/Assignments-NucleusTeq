c3ecb9f3cca5bd70cb9c6fa94094d851b45a5324
mastercj26 <cj450483@gmail.com>
2026-06-24 08:39:37 +0530

    feat: testing done

---
+++ ChetanJain_python_training/python_advance/run_datascience_demos.py
@@ -0,0 +1,31 @@
+
+
+from python_advance.NumPy.models import run_numpy_basics_demo
+from python_advance.NumPy.pandas_dataframe import run_pandas_dataframe_demo
+from python_advance.NumPy.data_cleaning import run_data_cleaning_demo
+from python_advance.NumPy.data_analysis import run_data_analysis_demo
+from python_advance.NumPy.matplotlib_charts import run_matplotlib_charts_demo
+from python_advance.NumPy.seaborn_visualizations import run_seaborn_visualizations_demo
+from python_advance.NumPy.mini_project import run_mini_project_demo
+
+
+if __name__ == "__main__":
+
+    run_numpy_basics_demo()
+
+
+    run_pandas_dataframe_demo()
+
+
+    run_data_cleaning_demo()
+
+
+    run_data_analysis_demo()
+
+    run_matplotlib_charts_demo()
+
+
+    run_seaborn_visualizations_demo()
+
+
+    run_mini_project_demo()
